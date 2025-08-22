a6884134dc97eaf9fde5f4aadf5d5c3e2354a337
Diana <dianakiyan2k20@gmail.com>
2025-08-22 18:30:06 +0300

    corrected the errors found by AI Buddy

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,12 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball ballFirst = lottery.getRandomBall();
-        Ball ballSecond = lottery.getRandomBall();
-        Ball ballThird = lottery.getRandomBall();
-
-        System.out.println(ballFirst);
-        System.out.println(ballSecond);
-        System.out.println(ballThird);
+        for (int i = 0; i < 3; i++) {
+            Ball ball = lottery.getRandomBall();
+            System.out.println(ball);
+        }
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private int ballId;
-
     final String getRandomColor() {
+        int ballId;
         ballId = new Random().nextInt(Colors.values().length);
         return Colors.values()[ballId].name();
     }
