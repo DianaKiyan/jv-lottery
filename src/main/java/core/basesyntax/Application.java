10686abf6a42a1c44dcc97d867f8eb5d77d1ae2b
Diana <dianakiyan2k20@gmail.com>
2025-08-22 18:19:11 +0300

    added a test for the program (creation of three random balls

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+        Lottery lottery = new Lottery();
+        Ball ballFirst = lottery.getRandomBall();
+        Ball ballSecond = lottery.getRandomBall();
+        Ball ballThird = lottery.getRandomBall();
 
+        System.out.println(ballFirst);
+        System.out.println(ballSecond);
+        System.out.println(ballThird);
     }
 }
